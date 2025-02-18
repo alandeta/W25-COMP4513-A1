2dc5275628b8a9b8ca1d1616921ae3256fba2716
alandeta <aland271@mtroyal.ca>
2025-02-17 17:42:23 -0700

    more routes

--- art-server.js
+++ art-server.js
@@ -91,6 +91,149 @@ app.get('/api/artists/country/:substring', async (req, res) => {
     res.send(data);
 });
 
+// all paintings
+app.get('/api/paintings', async (req, res) =>{
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .order('title', {ascending:true});
+    res.send(data);
+});
+
+// all paintings sorted by title or yearOfWork
+app.get('/api/paintings/sort/:method', async (req, res) => {
+    const { method } = req.params; 
+
+    if (method !== 'title' && method !== 'year') {
+        return res.send({ error: "Invalid sorting method. Must be 'year' or 'title'." });
+    }
+
+    const sortField = method === 'year' ? 'yearOfWork' : 'title';
+
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+            artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+            galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .order(sortField, { ascending: true });
+    res.send(data);
+});
+
+// returns specified painting by id
+app.get('/api/paintings/:ref', async (req, res) =>{
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .eq('paintingId', req.params.ref)
+        .order('title', {ascending:true});
+    if (data.length === 0) {
+        return res.send({error: `No paintings with painting ID ${req.params.ref} found. `})
+    }
+    res.send(data);
+});
+
+// return paintings with title beginning with substring
+app.get('/api/paintings/search/:substring', async (req, res) => {
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .ilike('title', `${req.params.substring}%`)
+        .order('title', {ascending:true});
+    if (data.length === 0){
+        return res.send({error: `No painting has a title starting with ${req.params.substring}. `})
+    }
+    res.send(data);
+});
+
+// return paintings between two years
+app.get('/api/paintings/years/:startYear/:endYear', async (req, res) => {
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .gte('yearOfWork', req.params.startYear) 
+        .lte('yearOfWork', req.params.endYear)
+        .order('yearOfWork', {ascending:true});
+    if (data.length === 0){
+        return res.send({error: `No paintings found in given year range.`})
+    }
+    res.send(data);
+});
+
+// return all paintings in given gallery
+app.get('/api/paintings/galleries/:ref', async (req, res) =>{
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .eq('galleryId', req.params.ref)
+        .order('title', {ascending:true});
+    if (data.length === 0) {
+        return res.send({error: `No paintings in gallery with gallery ID ${req.params.ref} found. `})
+    }
+    res.send(data);
+});
+
+// returns all paintings by artist
+app.get('/api/paintings/artist/:ref', async (req, res) =>{
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .eq('artistId', req.params.ref)
+        .order('title', {ascending:true});
+    if (data.length === 0) {
+        return res.send({error: `No paintings by artist with artist ID ${req.params.ref} found. `})
+    }
+    res.send(data);
+});
+
+// return all paintings by artists whose nationality begins with substring
+//THIS DOESNT WORK
+app.get('/api/paintings/artists/country/:ref', async (req, res) =>{
+    const { data, error } = await supabase
+        .from('paintings')
+        .select(`paintingId, imageFileName, title, museumLink, accessionNumber,
+            copyrightText, description, excerpt, yearOfWork, width, height, 
+            medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
+             artists:artistId (artistId, firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink),
+             galleries:galleryId (galleryId, galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)`)
+        .filter('artists.nationality', 'ilike', `${req.params.ref}%`)
+        .is('artists', 'not', null)
+        .order('title', {ascending:true});
+    if (!data || data.length === 0) {
+        return res.send({error: `No paintings by an artist with nationality starting with ${req.params.ref} found. `})
+    }
+    res.send(data);
+});
+
+
+
+
+
 
 
 
